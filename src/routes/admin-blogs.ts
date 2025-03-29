6f6b678875052baff0539584246bf96581837b42
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-03-29 16:36:19 +0530

    feat(admin): add view blog page

--- src/routes/admin-blogs.ts
+++ src/routes/admin-blogs.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { requireAuth } from "@clerk/express";
 import { hasAdminPerms } from "../middlewares";
-import { getAllBlogs } from "../services/blogs";
+import { getAllBlogs, getBlog } from "../services/blogs";
 
 const adminBlogsRouter = Router();
 
@@ -10,6 +10,18 @@ adminBlogsRouter.use(requireAuth({ signInUrl: "/sign-in" }), hasAdminPerms);
 adminBlogsRouter.get("/", (_, res) => {
   res.locals.layout = "admin-main";
   res.render("admin-blog-list", { blogs: getAllBlogs() });
+});
+
+adminBlogsRouter.get("/:id", (req, res, next) => {
+  const blog = getBlog(req.params.id);
+
+  if (blog) {
+    res.locals.layout = "admin-main";
+    res.render("admin-blog-view", { blog });
+    return;
+  }
+
+  next();
 });
 
 export { adminBlogsRouter };
